9a1772a013d853074130ab39810c66a344274061
Patrick Perez <patrickperez1698@gmail.com>
2022-07-16 01:03:51 -0300

    feat: sistema bytebank

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,74 @@
+﻿using ByteBank.Titular;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular{ get; set; }
+        public string Conta { get; set; }
+        public int Numero_agencia { get; set; }
+        public string Nome_agencia { get; set; }
+
+        private double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if(saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {               
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+        public void Despositar(double valor)
+        {
+            if(valor < 0)
+            {
+                return;
+            }
+            saldo = saldo + valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if(saldo < valor)
+            {
+                return false;
+            }
+            if(valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+            set
+            {
+               if(value < 0)
+                {
+                    return;
+                }
+                saldo = value;
+
+            }
+        }
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,15 @@
+﻿using ByteBank;
+using ByteBank.Titular;
+
+Console.WriteLine("Boas vindas ao seu banco, ByteBank!");
+
+Cliente sarah = new Cliente();
+
+
+ContaCorrente conta4 = new ContaCorrente();
+
+conta4.Saldo = 100;
+conta4.Titular = sarah;
+Console.WriteLine($"Saldo: {conta4.Saldo}");
+
+Console.ReadKey();
